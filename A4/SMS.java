edfbb60202b59547a74c4ce661cfb4d844afd6ed
Joshua Farwig <54459705+JoshFarwig@users.noreply.github.com>
2023-03-02 23:18:23 -0800

    removing personal auth ids

--- A4/SMS.java
+++ A4/SMS.java
@@ -3,8 +3,8 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 public class SMS {
-    public static final String ACCOUNT_SID = "AC7279ab828c97ab80bd482e8359b18652"; 
-    public static final String AUTH_TOKEN = "0c9d15d6f0b0f7886296dbdb898fa307";  
+    public static final String ACCOUNT_SID = ""; 
+    public static final String AUTH_TOKEN = "";  
 
     public void sendMessage(String phoneNumber, String MSG) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
